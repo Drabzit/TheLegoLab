96a797aff7e03e38ebff929cf2077e5dd370e0ae
Chris Bardsley <119276876+Drabzit@users.noreply.github.com>
2025-05-05 19:06:11 +0800

    qol

--- scripts/script.js
+++ scripts/script.js
@@ -18,4 +18,12 @@ document.addEventListener('DOMContentLoaded', () => {
       previewSection.style.display = 'block';
     });
   });
-  
+
+  window.onscroll = function () {
+    var button = document.querySelector('.back-to-top');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+}
